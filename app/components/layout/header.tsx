f984f76bb2257e3e7478d291cb930e50bdd46412
Julian Inwood <jinw@protonmail.com>
2024-02-14 09:18:58 +0000

    Fontawesome + sign out

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -1,5 +1,12 @@
 "use client";
+
+import { useAuth, useUser } from "@clerk/nextjs";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSign, faSignIn, faSignOut } from "@fortawesome/free-solid-svg-icons";
+
 const Header = () => {
+  const { isLoaded, isSignedIn } = useUser();
+  const { signOut } = useAuth();
   return (
     <header className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
@@ -21,6 +28,17 @@ const Header = () => {
                 Contact
               </a>
             </li>
+            {isLoaded && isSignedIn ? (
+              <li className="cursor-pointer">
+                <button onClick={() => signOut()}>
+                  <FontAwesomeIcon icon={faSignOut} />
+                </button>
+              </li>
+            ) : (
+              <li className="cursor-pointer">
+                <FontAwesomeIcon icon={faSignIn} />
+              </li>
+            )}
           </ul>
         </nav>
       </div>
